2e5c2107c24b29370f114d779acc39fad5d84e61
James Berg <james.berg38@yahoo.com>
2020-04-29 17:40:33 -0500

    added table data and loaded data into props and state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,61 @@ import React from "react";
 import Table from "./components/Table"
 
 function App() {
-  return <Table />;
+
+  const arrObjEmployees = 
+    [
+      {
+        first_name: "John",
+        last_name: "Doe",
+        role: "Sales Lead",
+        salary: "10000",
+        department: "Sales"
+      },
+      {
+        first_name: "Mike",
+        last_name: "Chan",
+        role: "Salesperson",
+        salary: "8000",
+        department: "Sales"
+      },
+      {
+        first_name: "Ashley",
+        last_name: "Rodriquez",
+        role: "Lead Engineer",
+        salary: "15000",
+        department: "Engineering"
+      },
+      {
+        first_name: "Kevin",
+        last_name: "Tupik",
+        role: "Software Engineer",
+        salary: "12000",
+        department: "Engineering"
+      },
+      {
+        first_name: "Malia",
+        last_name: "Brown",
+        role: "Accountant",
+        salary: "12500",
+        department: "Finance"
+      },
+      {
+        first_name: "Sarah",
+        last_name: "Lourd",
+        role: "Legal Team Lead",
+        salary: "25000",
+        department: "Legal"
+      },
+      {
+        first_name: "Tom",
+        last_name: "Allen",
+        role: "Lawyer",
+        salary: "18000",
+        department: "Legal"
+      }
+    ]
+
+  return <Table employees={arrObjEmployees}/>;
 }
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,42 @@
+// import React component
 import React, { Component } from "react";
 
+// START Tale class component
 class Table extends Component{
+    
+    // START Table class component constructor
+    constructor(props){
+        
+        // pass props to base class constructor
+        super(props);
+
+    }
+    // END Table class component constructor
+
+    // START define state objects
     state = {
-
+        employees: this.props.employees,
+        columns: Object.keys(this.props.employees[0])
     };
+    // END define state objects
 
+    // START Table class render method
     render() {
         return (
-            <div>Hello World</div>
+            <table>
+                {/* TODO: create Thead compenent */}
+                <thead>
+                    {this.state.columns.map(column => (
+                      <td>{column}</td>  
+                    ))}
+                </thead>
+            </table>
         );
     }
+    // END Table class render method
 
 }
+// END Tale class component
 
+// export table component
 export default Table;
