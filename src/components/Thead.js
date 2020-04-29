59e3dda9dca07dfd2a7e953377e76cdb0d06697c
James Berg <james.berg38@yahoo.com>
2020-04-29 18:18:34 -0500

    defined what Thead returns

--- src/components/Thead.js
+++ src/components/Thead.js
@@ -2,6 +2,16 @@ import React from "react";
 
 function Thead(props){
 
+    return(
+
+        <tr>
+            {props.columns.map(column => (
+                <td>{column}</td> 
+            ))} 
+        </tr>
+
+    );
+
 }
 
 export default Thead;
