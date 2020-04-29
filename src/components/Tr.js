3ba065827ac677c25ef21815b03a26f6361eb0c7
James Berg <james.berg38@yahoo.com>
2020-04-29 18:09:41 -0500

    defined what Tr returns

--- src/components/Tr.js
+++ src/components/Tr.js
@@ -3,6 +3,15 @@ import React, { Component } from "react";
 
 function Tr(props){
 
+    return (
+        <tr>
+            <td>{props.firstName}</td>
+            <td>{props.lastName}</td>
+            <td>{props.role}</td>
+            <td>{props.salary}</td>
+            <td>{props.department}</td>
+        </tr>
+    );
 
 }
 
