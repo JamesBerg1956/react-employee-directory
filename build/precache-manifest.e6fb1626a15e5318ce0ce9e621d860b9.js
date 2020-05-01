aeda5a2cac6048fec574c5c2f52d6e683d9ef86d
James Berg <james.berg38@yahoo.com>
2020-05-01 18:08:04 -0500

    this might make react work

---
+++ build/precache-manifest.e6fb1626a15e5318ce0ce9e621d860b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a1f959efcd5962b948f8c70431f333",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "808952fbed8016c3d1f2",
+    "url": "/react-employee-directory/static/js/2.f37ea6f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.f37ea6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31993ac37fc3f0e0805",
+    "url": "/react-employee-directory/static/js/main.427e73fe.chunk.js"
+  },
+  {
+    "revision": "4a1ef594bb52ab34bbe2",
+    "url": "/react-employee-directory/static/js/runtime-main.ed68bb1f.js"
+  }
+]);
