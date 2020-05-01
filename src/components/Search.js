a3e994ed3f764f7be6baf27b5832e98c6d778e11
James Berg <james.berg38@yahoo.com>
2020-05-01 08:18:09 -0500

    created src/components/Search.js

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+// import React component
+import React, { Component } from "react";
+
+function Search(props){
+
+    return(
+        <div>Hello World</div>
+    );
+
+}
+
+export default Search;
